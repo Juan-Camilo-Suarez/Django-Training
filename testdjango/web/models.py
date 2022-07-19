4c9ab7a43bfb2a6f51dd27ebfe64510cc233cc5f
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-19 22:26:34 +0300

    first setting and model site

--- testdjango/web/models.py
+++ testdjango/web/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+# para crear los usuarios de django
+User = get_user_model()
+
+
+class Site(models.Model):
+    url = models.URLField()
+    name = models.CharField(max_length=200)
+    status = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # auto_now_add=True sirve para que se crea automatico cuando se cree un modelo
+    create_at = models.DateTimeField(auto_now_add=True)
+    # se actualiza con la fecha del momento de la actualizacion
+    update_at = models.DateTimeField(auto_now=True)
+
