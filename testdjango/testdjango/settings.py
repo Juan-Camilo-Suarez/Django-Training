7ad54277771deacc5ffbc5a9c2fcd3405ba6c63b
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-22 09:27:35 +0300

    user model

--- testdjango/testdjango/settings.py
+++ testdjango/testdjango/settings.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+from django.conf.global_settings import AUTH_USER_MODEL
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,7 +27,6 @@ SECRET_KEY = 'django-insecure-p!9fb+*awa9qgi!b$mfbm5+w3cdxbdg)k#9f)ixwuw6xbyhkzq
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'testdjango.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -85,7 +84,6 @@ DATABASES = {
 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -105,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -117,13 +114,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "web.User"
--- testdjango/web/models.py
+++ testdjango/web/models.py
@@ -1,9 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import UserManager as DjangoUserManager, AbstractUser, PermissionsMixin
 from django.db import models
 
 # para crear los usuarios de django
-User = get_user_model()
-
 class BaseModel(models.Model):
     # auto_now_add=True sirve para que se crea automatico cuando se cree un modelo
     create_at = models.DateTimeField(auto_now_add=True)
@@ -11,11 +11,40 @@ class BaseModel(models.Model):
     update_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        #para que no se agrege este modelo a la base de datos
+        # para que no se agrege este modelo a la base de datos
         abstract = True
 
-#implementa BaseModel para agregar sus atributos a este modelos siendo base model
-#base model clase padre
+    # para definir los metodos para crear le usuario y superusuario
+
+
+class UserManager(DjangoUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        # en vez de username se usa email
+        user = self.model(email=username)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        user = self.model(email=username, is_staff=True, is_superuser=True)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+# implementa BaseModel para agregar sus atributos a este modelos siendo base model
+# base model clase padre
+
+class User(BaseModel, AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+    # de esta manera se puede decir que atributo va hacer de username
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=200)
+    # regula quien puede ir al admin
+    is_staff = models.BooleanField()
+
+
 class Site(BaseModel):
     url = models.URLField()
     name = models.CharField(max_length=200)
@@ -27,4 +56,3 @@ class SiteHistory(BaseModel):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     status_code = models.IntegerField()
     error_response_content = models.TextField(null=True, blank=True)
-
