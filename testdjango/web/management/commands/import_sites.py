a343217df9154279022af987fc51ab18edfd9fc7
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-25 11:40:26 +0300

    uptimizate with bulk_create

--- testdjango/web/management/commands/import_sites.py
+++ testdjango/web/management/commands/import_sites.py
@@ -11,12 +11,15 @@ class Command(BaseCommand):
         user_id = 10
 
         count = 0
+        sites = []
 
         for line in fd:
             line = line.strip()
             name = line.replace('https://', '').replace('http://', '').strip('/')
             site = Site(url=line, name=name, user_id=user_id)
-            site.save()
+            sites.append(site)
             count += 1
+
+        Site.objects.bulk_create(sites)
 
         print(f'{count} sites added')
