45bd2580094e80da3ed56586cdf3338e82dc75ed
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-26 17:16:15 +0300

    css in django static

--- testdjango/testdjango/urls.py
+++ testdjango/testdjango/urls.py
@@ -20,8 +20,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
                   path('', include('web.urls')),
                   path('admin/', admin.site.urls),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+              + static(settings.STATIC_URL)
