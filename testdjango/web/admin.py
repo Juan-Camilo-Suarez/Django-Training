61cc063907f7503bb1b6e5e8580052a96c38ffb1
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-28 14:41:21 +0300

    + site admin - read only fields

--- testdjango/web/admin.py
+++ testdjango/web/admin.py
@@ -6,11 +6,13 @@ from web.models import Site
 
 # como se va a ver la lista en el admin
 class SiteModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'url', 'status')
+    list_display = ('id', 'name', 'url', 'status', 'create_at', 'update_at')
     # convertir parametros en links
     list_display_links = ('id', 'name')
     list_filter = ('status',)
     search_fields = ('id', 'name', 'url')
+    # convierte estos params en no editables
+    readonly_fields = ('status', 'create_at', 'update_at')
 
 
 admin.site.register(Site, SiteModelAdmin)
