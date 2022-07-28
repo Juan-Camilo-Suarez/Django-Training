16590b9886015b9846d18823f80ddeac892503ad
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-28 14:31:20 +0300

    site admin - list display

--- testdjango/web/admin.py
+++ testdjango/web/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from web.models import Site
 
-admin.site.register(Site)
+
+# como se va a ver la lista en el admin
+class SiteModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url', 'status')
+    list_display_links = ('id', 'name')
+
+
+admin.site.register(Site, SiteModelAdmin)
