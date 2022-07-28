f0bc0de324d6e2d33698cae5c3fa12630f1e31b2
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-28 14:33:35 +0300

    site admin - filters & search

--- testdjango/web/admin.py
+++ testdjango/web/admin.py
@@ -7,7 +7,10 @@ from web.models import Site
 # como se va a ver la lista en el admin
 class SiteModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'url', 'status')
+    # convertir parametros en links
     list_display_links = ('id', 'name')
+    list_filter = ('status',)
+    search_fields = ('id', 'name', 'url')
 
 
 admin.site.register(Site, SiteModelAdmin)
