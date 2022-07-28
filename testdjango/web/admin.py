1114b78f98d96917808a8d3b41f3a196e59e15b9
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-28 14:54:34 +0300

    site history inline add model on other model in admin

--- testdjango/web/admin.py
+++ testdjango/web/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
-from web.models import Site
+from web.models import Site, SiteHistory
 
 
 # como se va a ver la lista en el admin
+class SiteHistoryInline(admin.TabularInline):
+    model = SiteHistory
+    readonly_fields = ('status_code', 'error_response_content', 'create_at')
+
+    # para no agregar y para no eliminar 
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+
 class SiteModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'url', 'status', 'create_at', 'update_at')
     # convertir parametros en links
@@ -13,9 +26,11 @@ class SiteModelAdmin(admin.ModelAdmin):
     search_fields = ('id', 'name', 'url')
     # convierte estos params en no editables
     readonly_fields = ('status', 'create_at', 'update_at')
-    #orden
-    ordering = ('-updated_at',)
-    #exclude() ayuda aquitar un param
+    # orden
+    ordering = ('-update_at',)
+    # exclude() ayuda aquitar un param
+    #agregar otro modelo que se conecta con este
+    inlines = (SiteHistoryInline,)
 
 
 admin.site.register(Site, SiteModelAdmin)
