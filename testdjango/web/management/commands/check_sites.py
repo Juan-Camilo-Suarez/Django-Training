82afea0aa104e361ad12f9bc1c19308554aa301b
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-28 13:25:40 +0300

    pereodic site check

--- testdjango/web/management/commands/check_sites.py
+++ testdjango/web/management/commands/check_sites.py
@@ -1,9 +1,20 @@
+import time
+
 from django.core.management import BaseCommand
 
 from web.services import check_sites
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        site_history = check_sites()
-        print(f'{len(site_history)} sites checked')
+    def add_arguments(self, parser):
+        # python manage.py check_sites --daemon true
+        parser.add_argument("--daemon", dest='daemon', help="Start daemon process", type=bool)
+
+    def handle(self, daemon, *args, **options):
+        while True:
+            site_history = check_sites()
+            print(f'{len(site_history)} sites checked')
+            if not daemon:
+                break
+            time.sleep(60)
+
